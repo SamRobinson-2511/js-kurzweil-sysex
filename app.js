1515466fd2ac7a54ec2b4928efb7ee05bdb8dc52
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2024-01-06 01:54:20 -0600

    midi outs listed in header

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 
 
 const connectButton = document.getElementById('connect')
-const listButton = document.getElementById('list');
-const buttons = document.querySelectorAll('.button')
-buttons.forEach(button => {
-  button.addEventListener('click', ()=> handleButton(event))
-});
 
-const keys = document.querySelectorAll('.key');
-keys.forEach(key => {
-  key.addEventListener('click', ()=> playNote(key));
-});
+
+
 
 
 
@@ -20,81 +13,97 @@ if (navigator.requestMIDIAccess) {
 
 
 function success (midiAccess){
-  console.log(midiAccess)
-  midiAccess.addEventListener('statechange', updateDevices)
+  midiAccess.addEventListener('statechange', updateDevices);
+
+  const inputs = midiAccess.inputs;
+  inputs.forEach(input => {
+    input.addEventListener('midimessage', handleInput);
+
+  })
+
+  const outputs = midiAccess.outputs;
+  const outputSelect = document.getElementById('selectOutport')
+  outputs.forEach(output => {
+    let option = output.name;
+    let outputPort = document.createElement('option');
+    outputPort.textContent = option;
+    outputPort.value = option;
+    outputSelect.appendChild(outputPort)
+    output.addEventListener('click', setOutPort)
+  })
+
+  const buttons = document.querySelectorAll('.button')
+  buttons.forEach(button => {
+    button.addEventListener('click', handleButton)
+  });
+
+  const keys = document.querySelectorAll('.key');
+  keys.forEach(key => {
+    key.addEventListener('click', playNote);
+  });
+
   
-  const inputPorts = midiAccess.inputs.values();
-  console.log(inputPorts)
-  for (const input of inputPorts){
-    console.table(
-      `
-      MIDI Input Port: ${input.name}
-      `
-      )
   }
-
-  const outputPorts = midiAccess.outputs.values();
-  for (const output of outputPorts){
-  // console.log(
-  //   `
-  //   MIDI Output Port: ${output.name}
-  //   MIDI Output ID: ${output.id}
-  //   Sysex: ${output.sysexEnabled}
-  //   `
-  //   )
-  
-  }
-}
-
-
-
-
 
 function updateDevices(event){
-   console.log(
-     `
-     Name:  ${event.port.name}
-     Brand: ${event.port.manufacturer} 
-     State: ${event.port.state}
-     Type: ${event.port.type}
-     Sysex: ${event.port}
-     `   
-    )
+
+  //  console.log(
+  //    `
+  //    Name:  ${event.port.name}
+  //    Brand: ${event.port.manufacturer} 
+  //    State: ${event.port.state}
+  //    Type: ${event.port.type}
+  //    Sysex: ${event.port}
+  //    `   
+  //   )
+}
+function failure(){
+  alert('could not connect')
 }
 
-function failure(){
-  console.log('could not connect')
+//select MIDI outport
+const setOutPort =(e)=>{
+  console.log(e.target.value)
 }
+
+
+
+
 
 const handleInput = (input) => {
   const command = input.data[0];
   const note = input.data[1];
   const velocity = input.data[2];
-  console.log(command, note, velocity);
 
   switch (command) {
     case 144: //note on 
     if (velocity > 0) {
       noteOn(note, velocity)
     } else {
-      noteOff(note);
+      noteOff(note, velocity);
     }
     break;
     case 128: //noteoff
-      noteOff();
+      noteOff(note, velocity);
       break;
   }
 }
 
+//callback functions 
 const handleButton = (event) => {
-  console.log(event.target.id)
+  console.log(event.target)
+
 }
 
 const noteOn = (note, velocity) => {
   console.log(note, velocity)
 }
+const playNote =(event)=>{
+  console.log(event.target)
+}
 
-const noteOff = (note) => {
+const noteOff = (note, velocity) => {
+  console.log(note, velocity)
 }
 
 const handleOutput = () => {
